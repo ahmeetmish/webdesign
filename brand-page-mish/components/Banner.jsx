d0abf82210c2df6990f23b3f04a29ff5bdc94806
ahmet <codingwithmish@gmail.com>
2024-06-23 21:17:34 +0300

    updated banner

--- brand-page-mish/components/Banner.jsx
+++ brand-page-mish/components/Banner.jsx
@@ -2,9 +2,12 @@ import Image from "next/image"
 
 export default function Banner() {
   return (
-    <section className="w-full">
+    <section className="w-full space-y-6">
       <div>
         <Image src="https://placehold.co/2000x400" alt="Banner" className="w-full h-[400px] object-cover" width={2000} height={400} />
+      </div>
+      <div>
+        <p className="text-4xl font-semibold">In modern web design, every new project is an opportunity to develop our creativity and offer better solutions. <br /> Always being on the move brings evolution and innovation.</p>
       </div>
     </section>
   )
