5f75485f6acf98fccc1de668dcc8d4d6499a011c
ahmet <codingwithmish@gmail.com>
2024-06-23 19:29:32 +0300

    fixed transition duration

--- brand-page-mish/components/Header.jsx
+++ brand-page-mish/components/Header.jsx
@@ -26,10 +26,10 @@ export default function Header() {
   }, [])
 
   return (
-    <header className={`sticky top-0 w-auto flex justify-between items-center py-4 header-glass-background ${scrolled && 'border-b-[1px] border-zinc-300'}`}>
+    <header className={`sticky top-0 w-auto flex justify-between items-center py-4 header-glass-background transition-all duration-200 ${scrolled && 'border-b-[1px] border-zinc-300'}`}>
       <div className="gap-6 w-full flex items-center justify-between">
         <div>
-          <Link href="/" className="w-max gap-2 flex items-center justify-center py-2 group transition-all hover:px-4 hover:bg-zinc-900 hover:rounded">
+          <Link href="/" className="w-max gap-2 flex items-center justify-center py-2 group transition-all duration-300 hover:px-4 hover:bg-zinc-900 hover:rounded">
             <Pyramid className="size-6 group-hover:text-white" />
             <h2 className="text-[1.3rem] font-semibold tracking-wide group-hover:text-white">mish</h2>
           </Link>
@@ -39,7 +39,7 @@ export default function Header() {
           <Link href="/" className="text-[1.3rem] font-semibold tracking-wide hover:!text-zinc-700 group-hover:text-zinc-900 transition-all">News</Link>
         </div>
         <div>
-          <Link href="/" className="w-max gap-2 flex items-center justify-center py-2 group transition-all hover:px-4 hover:bg-zinc-900 hover:rounded">
+          <Link href="/" className="w-max gap-2 flex items-center justify-center py-2 group transition-all duration-300 hover:px-4 hover:bg-zinc-900 hover:rounded">
             <span className="text-[1.3rem] font-semibold tracking-wide group-hover:text-white">Contact</span>
             <MoveUpRight className="size-6 group-hover:text-white" />
           </Link>
