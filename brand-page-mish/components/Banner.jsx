8adf8ffe59b18eb5fbad73d917acbe2d3405303f
ahmet <codingwithmish@gmail.com>
2024-06-23 22:29:40 +0300

    updated images

--- brand-page-mish/components/Banner.jsx
+++ brand-page-mish/components/Banner.jsx
@@ -4,7 +4,7 @@ export default function Banner() {
   return (
     <section className="w-full space-y-6">
       <div>
-        <Image src="https://placehold.co/2000x400" alt="Banner" className="w-full h-[400px] object-cover" width={2000} height={400} />
+        <Image src="/1.jpg" alt="Banner" className="w-full h-[400px] object-cover" width={2000} height={400} />
       </div>
       <div>
         <p className="text-4xl font-semibold">In modern web design, every new project is an opportunity to develop our creativity and offer better solutions. <br /> Always being on the move brings evolution and innovation.</p>
--- brand-page-mish/components/Projects/Projects.jsx
+++ brand-page-mish/components/Projects/Projects.jsx
@@ -5,16 +5,16 @@ export default function Projects() {
     <section>
       <div className="gap-y-20 flex flex-col md:grid md:grid-cols-14">
         <div className="col-span-9 col-start-12 max-[768px]:w-full">
-          <Card image="https://placehold.co/800x600" alt="Project" width={800} height={600} title="Lorem Ipsum" description="Lorem" />
+          <Card image="/2.jpg" alt="Project" width={800} height={600} title="Lorem Ipsum" description="Lorem" />
         </div>
         <div className="col-span-10 max-[768px]:w-full">
-          <Card image="https://placehold.co/900x700" alt="Project" width={900} height={700} title="Lorem Ipsum" description="Lorem" />
+          <Card image="/3.jpg" alt="Project" width={900} height={700} title="Lorem Ipsum" description="Lorem" />
         </div>
         <div className="col-span-8 col-start-13 md:mt-[50rem] max-[768px]:w-full">
-          <Card image="https://placehold.co/700x700" alt="Project" width={700} height={700} title="Lorem Ipsum" description="Lorem" />
+          <Card image="/4.jpg" alt="Project" width={700} height={700} title="Lorem Ipsum" description="Lorem" />
         </div>
         <div className="col-span-12 col-start-1 md:mt-10 max-[768px]:w-full">
-          <Card image="https://placehold.co/1200x600" alt="Project" width={1200} height={600} title="Lorem Ipsum" description="Lorem" />
+          <Card image="/5.jpg" alt="Project" width={1200} height={600} title="Lorem Ipsum" description="Lorem" />
         </div>
       </div>
     </section>
