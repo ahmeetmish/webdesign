e524b7a94ebb4c8e631ae6eaa560eac439ad64c3
ahmet <codingwithmish@gmail.com>
2024-06-23 13:31:45 +0300

    created template with nextjs

---
+++ brand-page-mish/app/layout.js
@@ -0,0 +1,18 @@
+import "./globals.css"
+import localFont from "next/font/local"
+
+export const metadata = {
+  title: "Mish Brand Page",
+}
+
+const ppneuemontreal = localFont({ src: '../font/ppneuemontreal-book.woff', weight: '400', style: 'normal' })
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={ppneuemontreal.className}>
+        {children}
+      </body>
+    </html>
+  )
+}
